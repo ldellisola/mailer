03574c16b802e29b514ac24895e24603b7b85f5b
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-12-06 21:01:24 -0300

    [Feature] Added email

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:7.0-alpine AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0-alpine AS build
+WORKDIR /src
+COPY ["Mailer.csproj", "./"]
+RUN dotnet restore "Mailer.csproj" 
+
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "Mailer.csproj" -c Release -o /app/build --no-restore
+
+FROM build AS publish
+RUN dotnet publish "Mailer.csproj" -c Release -o /app/publish --no-restore
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet","Mailer.dll"]
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CommandLine.DragonFruit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace Mailer;
+
+internal class Program
+{
+    /// <summary>
+    /// A simple tool to send an email using smtp.
+    /// </summary>
+    /// <param name="fromAddress">Email address of the sender</param>
+    /// <param name="toAddress">Email address of the receiver</param>
+    /// <param name="subject">Subject of the email</param>
+    /// <param name="content">Text content of the email</param>
+    /// <param name="password">Password for the email provider</param>
+    /// <param name="username">Username for the email provider</param>
+    /// <param name="fromName">Name of the sender. It default to the sender's email</param>
+    /// <param name="toName">Name of the receiver. It defaults to the receiver's email</param>
+    /// <param name="server">Address of the email provider</param>
+    /// <param name="port">Port of the email provider</param>
+    /// <param name="useSsl">Whether to use ssl</param>
+    private static void Main(
+        string fromAddress, string toAddress, string subject, string content,
+        string password, string? username = null,
+        string? fromName = null,
+        string? toName = null,
+        string server = "smtp.gmail.com",
+        int port = 587,
+        bool useSsl = false
+    )
+    {
+        try
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(fromName ?? fromAddress, fromAddress));
+            message.To.Add(new MailboxAddress(toName ?? toAddress, toAddress));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = content
+            };
+
+            using var client = new SmtpClient();
+            client.Connect(server, port, useSsl);
+            client.Authenticate(username ?? fromAddress, password);
+            client.Send(message);
+            client.Disconnect(true);
+        }
+        catch (AuthenticationException)
+        {
+            Console.WriteLine("Authentication failed");
+        }
+    }
+}
